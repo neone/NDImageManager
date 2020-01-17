5cdb35b2412cb06bc8852f2188f2e88a400d7e70
Dave Glassco <dave@neone.com>
2020-01-17 16:29:37 -0600

    removed inits

--- Sources/NDImageManager/NDImageManager.swift
+++ Sources/NDImageManager/NDImageManager.swift
@@ -27,11 +27,6 @@ public class NDImageManager: UIViewController, UIImagePickerControllerDelegate, 
         launchImagePicker()
     }
     
-    public init() {}
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     /// Public Setup Method - this is how NDImageManager should be set from outside the framework
     /// - Parameters:
