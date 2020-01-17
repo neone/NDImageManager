4637dcf624dd5a24342b9c83fd7e11bd4e5c0a3b
Dave Glassco <dave@neone.com>
2020-01-17 15:47:07 -0600

    initial setup

--- Sources/NDImageManager/NDImageManager.swift
+++ Sources/NDImageManager/NDImageManager.swift
@@ -1,3 +1,103 @@
-struct NDImageManager {
-    var text = "Hello, World!"
+//
+//  NDImageManager.swift
+//  NDImageManager
+//
+//  Created by Dave Glassco on 1/17/20.
+//  Copyright © 2020 Neone. All rights reserved.
+//
+
+import UIKit
+
+protocol NDImagePickerDelegate {
+    func editedImageReturned(image: UIImage)
+}
+
+class NDImageManager: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CropperViewControllerDelegate {
+
+    var cropperState: CropperState?
+    fileprivate var shouldShowEdit = false
+    fileprivate var isRounded = false
+    
+    var imagePickerDelegate: NDImagePickerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        launchImagePicker()
+    }
+    
+    
+    /// Public Setup Method - this is how NDImageManager should be set from outside the framework
+    /// - Parameters:
+    ///   - editable: sets whether edit window called after picker
+    ///   - rounded: set edit window crop to round only
+    public func setUpImageManager(editable: Bool, rounded: Bool ) {
+        if editable {
+            shouldShowEdit = true
+        }
+        if rounded {
+            isRounded = true
+        }
+    }
+    
+    
+    //Private Class Methods
+    fileprivate func launchImagePicker() {
+        let picker = UIImagePickerController()
+               picker.sourceType = .photoLibrary
+               picker.allowsEditing = false
+               picker.delegate = self
+               present(picker, animated: true, completion: nil)
+    }
+}
+
+
+//MARK: ImagePickerDelegate
+extension NDImageManager {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+           guard let image = (info[.originalImage] as? UIImage) else { return }
+        
+        //Setup the QCropper View
+        let cropper = CropperViewController(originalImage: image)
+        cropper.delegate = self
+        
+        if isRounded {
+            cropper.isCircular = true
+        }
+    
+            //Dismiss system picker and launch options if selected
+        picker.dismiss(animated: true) {
+            if self.shouldShowEdit {
+                self.present(cropper, animated: true, completion: nil)
+            } else {
+                self.imagePickerDelegate?.editedImageReturned(image: image)
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+//MARK: CropperViewDelegate
+extension NDImageManager {
+    func cropperDidConfirm(_ cropper: CropperViewController, state: CropperState?) {
+        cropper.dismiss(animated: true, completion: nil)
+
+        if let state = state,
+            let image = cropper.originalImage.cropped(withCropperState: state) {
+            cropperState = state
+            imagePickerDelegate?.editedImageReturned(image: image)
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func cropperDidCancel(_ cropper: CropperViewController) {
+        cropper.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- Tests/NDImageManagerTests/NDImageManagerTests.swift
+++ Tests/NDImageManagerTests/NDImageManagerTests.swift
@@ -6,7 +6,7 @@ final class NDImageManagerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NDImageManager().text, "Hello, World!")
+        
     }
 
     static var allTests = [
